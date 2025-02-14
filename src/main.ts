f7a48d6075f3c5a457ee38e82fbff92ffc0a6741
lgoliveira <lucas.guimaraes.lc@gmail.com>
2025-02-13 23:50:47 -0300

    Add Cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Medical Insurance API')
     .setDescription('API for a medical insurance company')
